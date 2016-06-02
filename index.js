281daf0149f2e23f94b6a013a4c1c100c9275960
Dennis Parsons <dennsprsns@gmail.com>
2016-06-02 11:47:50 +0300

    to work with onError attribute for default image

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 				}
 
 				$scope.$watch('objectURL', function (objectURL) {
-					elem.attr('src', objectURL);
+					elem.attr('src', objectURL || attrs.onError);
 				});
 
 				$scope.$on('$destroy', function () {
@@ -29,7 +29,7 @@
 						$http.get(url, { responseType: 'arraybuffer' })
 							.then(function (response) {
 								var blob = new Blob(
-									[ response.data ], 
+									[ response.data ],
 									{ type: response.headers('Content-Type') }
 								);
 								$scope.objectURL = URL.createObjectURL(blob);
